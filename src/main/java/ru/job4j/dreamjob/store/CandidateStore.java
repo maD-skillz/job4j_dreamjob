1136c97ae5b911d693cb655d8eea61e27b952b2b
maD-skillz <warneverchang3s@gmail.com>
2022-04-17 14:40:51 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,29 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1,"Junior Java Developer", "Junior Java Candidate", LocalDateTime.now()));
+        candidates.put(2, new Candidate(2,"Middle Java Developer", "Middle Java Candidate", LocalDateTime.now()));
+        candidates.put(3, new Candidate(3,"Senior Java Developer", "Senior Java Candidate", LocalDateTime.now()));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
