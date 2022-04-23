17a5c5459c1443eae5f5e05c4172639edbbecc94
maD-skillz <warneverchang3s@gmail.com>
2022-04-23 18:07:13 +0300

    3. PostController.createPost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,6 +3,8 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
 
@@ -10,7 +12,20 @@ import java.time.LocalDateTime;
 
 @Controller
 public class PostController {
+
     private final PostStore store = PostStore.instOf();
+
+
+    @GetMapping("/addPost")
+    public String addPost(Model model) {
+        model.addAttribute("post", new Post(0, "Заполните поле", "Заполните текст", LocalDateTime.now()));
+        return "addPost";
+    }
+
+    @GetMapping("/formAddPost")
+    public String formAddPost(Model model) {
+        return "addPost";
+    }
 
     @GetMapping("/posts")
     public String posts(Model model) {
@@ -18,9 +33,12 @@ public class PostController {
         return "posts";
     }
 
-    @GetMapping("/addPost")
-    public String addPost(Model model) {
-        model.addAttribute("post", new Post(0, "Заполните поле", "Заполните текст", LocalDateTime.now()));
-        return "addPost";
+    @PostMapping("/createPost")
+    public String createPost(@ModelAttribute Post post) {
+        store.add(post);
+        return "redirect:/posts";
     }
+
 }
+
+
--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -1,18 +1,23 @@
 package ru.job4j.dreamjob.store;
 
+
 import ru.job4j.dreamjob.model.Post;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class PostStore {
     private static final PostStore INST = new PostStore();
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
-    private PostStore() {
+    private final AtomicInteger ids = new AtomicInteger(4);
+
+    public PostStore() {
         posts.put(1, new Post(1, "Junior Java Job", "Junior Java vacancy", LocalDateTime.now()));
         posts.put(2, new Post(2, "Middle Java Job", "Middle Java vacancy", LocalDateTime.now()));
         posts.put(3, new Post(3, "Senior Java Job", "Senior Java vacancy", LocalDateTime.now()));
@@ -25,4 +30,11 @@ public class PostStore {
     public Collection<Post> findAll() {
         return posts.values();
     }
+
+    public void add(Post post) {
+        post.setId(ids.incrementAndGet());
+        post.setCreated(LocalDateTime.now());
+        posts.put(post.getId(), post);
+    }
+
 }
