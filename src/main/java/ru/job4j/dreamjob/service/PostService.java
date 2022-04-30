512fc29bc036659a472a47f0749ca355eb793fc4
maD-skillz <warneverchang3s@gmail.com>
2022-04-30 16:49:21 +0300

    1. Слоеная архитектура. [#504851]

--- src/main/java/ru/job4j/dreamjob/service/PostService.java
+++ src/main/java/ru/job4j/dreamjob/service/PostService.java
@@ -1,15 +1,34 @@
 package ru.job4j.dreamjob.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
 
+
 import java.util.Collection;
+
 
 @Service
 public class PostService {
 
+    private final PostStore store = PostStore.instOf();
+
+    public Post findPostById(int id) {
+        return store.findById(id);
+    }
+
+    public void addPost(Post post) {
+        store.add(post);
+    }
+
+    public Collection<Post> findAllPosts() {
+        return store.findAll();
+    }
+
+    public void updatePost(Post post) {
+        store.update(post);
+    }
 
 }
 
