e5f2b1e5704428fa009017d1342c50c844aabec5
maD-skillz <warneverchang3s@gmail.com>
2022-05-21 17:12:19 +0300

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859]

--- src/main/java/ru/job4j/dreamjob/service/PostService.java
+++ src/main/java/ru/job4j/dreamjob/service/PostService.java
@@ -1,22 +1,24 @@
 package ru.job4j.dreamjob.service;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Post;
-import ru.job4j.dreamjob.store.PostStore;
-
+import ru.job4j.dreamjob.store.PostDbStore;
 
 import java.util.Collection;
+import java.util.List;
 
 
 @Service
 public class PostService {
 
-    private final PostStore store;
+    private final PostDbStore store;
 
-    public PostService(PostStore store) {
-       this.store = store;
+    private final CityService cityService;
+
+    public PostService(PostDbStore store, CityService cityService) {
+        this.store = store;
+        this.cityService = cityService;
     }
 
     public Post findPostById(int id) {
@@ -28,6 +30,16 @@ public class PostService {
     }
 
     public Collection<Post> findAllPosts() {
+        return store.findAll();
+    }
+
+    public List<Post> findAllCities() {
+        List<Post> posts = store.findAll();
+        posts.forEach(
+                post -> post.setCity(
+                        cityService.findById(post.getCity().getId())
+                )
+        );
         return store.findAll();
     }
 
---
+++ src/main/java/ru/job4j/dreamjob/store/PostDbStore.java
@@ -0,0 +1,106 @@
+package ru.job4j.dreamjob.store;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.City;
+import ru.job4j.dreamjob.model.Post;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PostDbStore {
+    private final BasicDataSource pool;
+
+    public PostDbStore(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public List<Post> findAll() {
+        List<Post> posts = new ArrayList<>();
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =
+                     cn.prepareStatement("SELECT * FROM post")
+        ) {
+            try (ResultSet it = ps.executeQuery()) {
+                while (it.next()) {
+                    posts.add(new Post(
+                            it.getInt("id"),
+                            it.getString("name"),
+                            it.getString("description"),
+                            LocalDateTime.now(),
+                            new City(1,"city")));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+
+    public Post add(Post post) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =
+                     cn.prepareStatement("INSERT INTO post(name, description, city_id) VALUES (?, ?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+            ps.setString(1, post.getName());
+            ps.setString(2, post.getDescription());
+            ps.setInt(3, post.getCity().getId());
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    post.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    public void update(Post post) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =
+                     cn.prepareStatement("UPDATE post SET name = ?, description = ? WHERE id = ?")
+        ) {
+            ps.setString(1, post.getName());
+            ps.setString(2, post.getDescription());
+            ps.setInt(3, post.getId());
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    post.setName(id.getString("name"));
+                    post.setDescription(id.getString("description"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Post findById(int id) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement("SELECT * FROM post WHERE id = ?")
+        ) {
+            ps.setInt(1, id);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    return new Post(
+                            it.getInt("id"),
+                            it.getString("name"),
+                            it.getString("description"),
+                            LocalDateTime.now(),
+                            new City());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
