f4dd0f7eb97269b7d8cf69054b58eb8a10d6c2b7
maD-skillz <warneverchang3s@gmail.com>
2022-05-24 20:12:37 +0300

    4. Многопоточность в базе данных [#504860 #304707]

--- src/main/java/ru/job4j/dreamjob/model/User.java
+++ src/main/java/ru/job4j/dreamjob/model/User.java
@@ -7,14 +7,15 @@ public class User implements Serializable {
 
     private int id;
 
-    private String name;
-
     private String email;
 
-    public User(int id, String name, String email) {
+    private String password;
+
+
+    public User(int id, String email, String password) {
         this.id = id;
-        this.name = name;
         this.email = email;
+        this.password = password;
     }
 
     public int getId() {
@@ -25,20 +26,20 @@ public class User implements Serializable {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
@@ -50,11 +51,12 @@ public class User implements Serializable {
             return false;
         }
         User user = (User) o;
-        return Objects.equals(name, user.name) && Objects.equals(email, user.email);
+        return id == user.id && Objects.equals(email, user.email)
+                && Objects.equals(password, user.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, email);
+        return Objects.hash(id, email, password);
     }
 }
